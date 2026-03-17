03d006948c5fd982227528b5bf5bb125e90211bb
minthuta007 <148946584+minthuta007@users.noreply.github.com>
2026-03-17 20:44:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ I know i have a lot of flaws and mistakes to fix but thank you for sticking with
 
 For you the promises I give right now are looking likely to break but the most absolute thing i can ensure you is that i will never give up on you and will learn how to love you more till the time ends. I still couldn’t self-reflect my ego but I will be willing to learn how to change for you.
 
-I will do anything for you but i will have apologize to you for all my shortcomings that I will do and I’ve done. I will love you more and more till my love exceeds yours ❤️.
+I will do anything for you but i have to apologize for all my shortcomings that I will do and I’ve done. I will love you more and more till my love exceeds yours ❤️.
 
 I hope that we can fix all the problems and overcome all the hardships together while holding hands side by side and loving each other more and more. I love you chit sone ❤️ Love you more than i can show and more than you feel ❤️🫶 Arrr bwarrrr 😘😘😘`;
 
